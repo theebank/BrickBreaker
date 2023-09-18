c2f422bb7ae2cfca12f08448a5d635252a61e5f4
theebank <tkumaresan88@gmail.com>
2023-09-18 14:20:41 -0400

    Initial Commit

---
+++ Brickbreaker/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  Brickbreaker
+//
+//  Created by Theeban Kumaresan on 2023-09-18.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
